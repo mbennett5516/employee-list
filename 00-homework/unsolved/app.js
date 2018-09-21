18cc18699280834a349cc067cc70c51ebd7c3d9f
mbennett <mbennett5516@yahoo.com>
2018-09-21 09:44:55 -0500

    stuff n things

--- 00-homework/unsolved/app.js
+++ 00-homework/unsolved/app.js
@@ -10,6 +10,7 @@ $('#view-link').on('click', function(){
         employee += '</p></div>';
         $("#info").append(employee);
     }
+    $('#updateBar').addClass('hide');
     $('#searchBar').addClass('hide');
     $('#addBar').addClass('hide');
 })
@@ -26,24 +27,19 @@ $('#add-link').on('click', function(){
         employee += '</p></div>';
         $("#info").append(employee);
     }
+    $('#updateBar').addClass('hide');
     $('#searchBar').addClass('hide');
     $('#addBar').removeClass('hide');
 })
 
 $('#submit').on('click', function(stopRefresh){
     stopRefresh.preventDefault();
-    // let employee = '<div class="empInfo"><p class="name">';
-    // employee += document.getElementById('name').value;
-    // employee += '</p><p class="officeNum">';
-    // employee += document.getElementById('officeNum').value;
-    // employee += '</p><p class="phone">';
-    // employee += document.getElementById('phone').value;
-    // employee += '</p></div>'
     let employee = {
         name: document.getElementById('name').value,
-        officeNum: document.getElementById('officeNum').value,
+        officeNum: parseInt(document.getElementById('officeNum').value),
         phoneNum: document.getElementById('phone').value
     };
+
     employeeList.unshift(employee);
     $('#info').empty();
     for(let i=0;i<employeeList.length;i++){
@@ -59,13 +55,34 @@ $('#submit').on('click', function(stopRefresh){
 })
 
 $('#verify-link').on('click', function(){
-
+    $('#info').empty();
+    $('#updateBar').addClass('hide');
     $('#searchBar').removeClass('hide');
     $('#addBar').addClass('hide');
 })
 
-$('update-link').on('click', function(){
+$('#searchButton').on('click', function(stopRefresh){
+    stopRefresh.preventDefault();
+    const name = document.getElementById('searchName').value;
+    for (let i=0;i<employeeList.length;i++){
+        if (name===employeeList[i].name){
+            alert(`${name} is already in the list!`);
+            return true;
+        }
+        else{
+            alert(`${name} is not in the list yet!`);
+            return false;
+        }
+    }
+})
 
+$('#update-link').on('click', function(){
+    $('#info').empty();
+    const name = document.getElementById('updateSearch').value;
+    for (let i=0;i<employeeList.length;i++){
+
+    }
+    $('#updateBar').removeClass('hide');
     $('#searchBar').addClass('hide');
     $('#addBar').addClass('hide');
 })
